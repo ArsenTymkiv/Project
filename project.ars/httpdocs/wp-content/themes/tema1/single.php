b8df185fdb6be44c4badbbf2e33be41ea699abbc
Arsen Tymkiv <arseny.tymkiv.ua@gmail.com>
2021-09-20 11:00:34 +0300

    Update

--- project.ars/httpdocs/wp-content/themes/tema1/single.php
+++ project.ars/httpdocs/wp-content/themes/tema1/single.php
@@ -4,7 +4,8 @@
     <h1 class="my-4 page-title"><?php echo wp_get_document_title(); ?></h1>
     <img class="img-fluid rounded" src="<?php echo get_the_post_thumbnail_url(null, 'full');?>" alt="<?php echo wp_get_document_title(); ?>">
     <p><small class="text-muted"><?the_time('j F Y');?> <?php the_tags('');?></small></p>
-    <?the_content();?>
+    <?php the_content();?>
     <?php comments_template(); ?>
 <?endwhile;?>
+
 <?php get_footer();?>
